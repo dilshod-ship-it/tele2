9fdeb688378fed6e76752677c1e97576cdbe4177
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-02 13:11:46 +0000

    Deploy dilshod-ship-it/tele2 to github.com/dilshod-ship-it/tele2.git:gh-pages

---
+++ precache-manifest.046995da9f54a0d9503c9f07db77a70e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3be984b8914c15630945146bc0e05443",
+    "url": "/tele2/index.html"
+  },
+  {
+    "revision": "0b80a72eb2db33db131d",
+    "url": "/tele2/static/css/main.3a580fa9.chunk.css"
+  },
+  {
+    "revision": "e5139657e01045480748",
+    "url": "/tele2/static/js/2.0bfd1891.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tele2/static/js/2.0bfd1891.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0b80a72eb2db33db131d",
+    "url": "/tele2/static/js/main.9e0191fc.chunk.js"
+  },
+  {
+    "revision": "1289a868035832ca9be3",
+    "url": "/tele2/static/js/runtime-main.435df064.js"
+  }
+]);
